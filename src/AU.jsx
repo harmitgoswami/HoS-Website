072642beb23b9fb5e555e1cc6ea2e385d63e6f8b
The-Goon-King <147094571+The-Goon-King@users.noreply.github.com>
2023-11-10 18:21:15 -0500

    Add files via upload

---
+++ src/AU.jsx
@@ -0,0 +1,20 @@
+import './AU.css'
+import Heading from './components/Heading'
+import Footer from './components/Footer'
+import Title from './components/Title'
+import AboutUs from './components/AboutUs'
+import AboutUs2 from './components/AboutUs2'
+
+export default function AU() {
+  return (
+    <main>
+			<Heading/>
+			<Title/>
+			<br></br>
+			<AboutUs/>
+			<br></br>
+			<AboutUs2/>
+			<Footer/>
+    </main>
+  )
+}
