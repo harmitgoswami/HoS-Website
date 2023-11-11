fdcd90da4325ece6571ae8e67ad0c3272ce5abf8
The-Goon-King <147094571+The-Goon-King@users.noreply.github.com>
2023-11-11 15:30:28 -0500

    Add files via upload

---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './style.css';
+
+class Footer extends Component {
+	render() {
+		return (
+			<footer>
+				<p>&copy;Humans of Skule</p>
+			</footer>
+		);
+	}
+}
+
+export default Footer;
