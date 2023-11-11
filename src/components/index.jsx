b6db31c630a633cfd7dcaabbc703fc16f0976353
The-Goon-King <147094571+The-Goon-King@users.noreply.github.com>
2023-11-11 15:33:19 -0500

    Add files via upload

---
+++ src/components/index.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './style.css';
+
+class Heading extends Component {
+	render() {
+		return (
+			<header>
+				<h1>Our Staff</h1>
+			</header>
+		);
+	}
+}
+
+export default Heading;
