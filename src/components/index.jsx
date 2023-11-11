4489048e331f01489bfafb83bc0e70fdd3c8c529
The-Goon-King <147094571+The-Goon-King@users.noreply.github.com>
2023-11-11 15:37:47 -0500

    Add files via upload

---
+++ src/components/index.jsx
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './style.css';
+
+class AboutUs extends Component {
+	render() {
+		return (
+			<section class="AboutUs">
+				<section class="AboutUsText">
+					<p>Humans Of Skule is a photoblog capturing stories of the University of Toronto Engineering Society - Skule™ community members and Faculty of Applied Science Members through photo-portraits, interviews and videography. Humans of Skule also teaches photography, videography, interviewing and leadership in unconventional engineering areas through a series of workshops, seminars and competitions to the Skule community members.
+			</p>
+				</section>
+				<section class="IntroImage">
+				</section>
+			</section>
+		);
+	}
+}
+
+export default AboutUs;
